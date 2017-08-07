8143098a353603190e596a76928c1798f238b5f1
gengjiarong <canyifenglin@gmail.com>
2017-08-07 16:24:55 +0800

    添加退出按钮

--- src/components/common/commontitle.vue
+++ src/components/common/commontitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="title-container">
-    <a class="menuIcon" href="javascript:void(0);" @click="handleMenu">
+    <a class="menuIcon" href="javascript:void(0)" @click="handleMenu">
     <span class="dac-nav-hamburger">
       <span class="dac-nav-hamburger-top"></span>
       <span class="dac-nav-hamburger-mid"></span>
@@ -8,12 +8,24 @@
     </span>
     </a>
     <!--<span class="title-font">{{titleStr}}</span>-->
-    <div style="width: 193px;display: flex; align-items: center;border-right: 1px solid rgba(0, 0, 0, 0.1);">
+    <div
+      style="width: 173px;display: flex; align-items: center;border-right: 1px solid rgba(0, 0, 0, 0.1);margin-left: 20px">
       <img src="../../assets/iii.png" style="width: 50px; height: 50px; border-radius: 50%"/>
       <span class="nameClass">{{username}}</span>
     </div>
     <div class="titleClass">{{titleStr}}</div>
-    <!--搜索、退出-->
+
+
+    <a href="javascript:void(0)" @click="settingShow = !settingShow">
+      <img class="settingClass" src="../../assets/settings.png"/>
+    </a>
+    <transition name="setting-content">
+      <ul v-show="settingShow" class="settingMenu">
+        <li class="itemClass" v-for="(item, i) in settingData" @click="selectItem(i)">{{item}}</li>
+      </ul>
+    </transition>
+
+
   </div>
 </template>
 <script>
@@ -22,12 +34,18 @@
     data: function () {
       return {
         titleStr: '',
-        username: '张三'
+        username: '张三',
+        settingShow: false,
+        settingData: ['设置', '退出']
       }
     },
     methods: {
       handleMenu: function () {
         this.$emit('menuShow', 'menu显示隐藏')
+      },
+      selectItem: function (index) {
+        alert(index)
+        this.settingShow = !this.settingShow
       }
     },
     created: function () {
@@ -35,14 +53,26 @@
     }
   }
 </script>
-<style>
+<style scoped>
+
+  li:hover {
+    background-color: #eeeeee;
+  }
+
+  .setting-content-active, .setting-content-leave-active {
+    transition: opacity .5s
+  }
+
+  .setting-content-enter, .setting-content-leave-to {
+    opacity: 0;
+  }
 
   .nameClass {
     font-family: "Helvetica", "Arial", sans-serif;
     font-size: 14px;
     font-weight: 400;
     padding-left: 20px;
-    margin-left: 20px;
+    margin-left: 10px;
     text-align: center;
     /*单行文本 打点显示*/
     display: block;
@@ -65,6 +95,7 @@
     line-height: 64px;
     font-family: "Roboto", "Helvetica", "Arial", sans-serif;
     font-size: 20px;
+    flex: 1;
     text-transform: capitalize;
     color: #fff;
   }
@@ -77,8 +108,7 @@
     background-color: #6ab344;
     display: flex;
     flex-direction: row;
-    /*justify-content: center;*/
-    /*align-items: center;*/
+    align-items: center;
     /*阴影*/
     vertical-align: middle;
     box-shadow: 2px 5px 6px #ddd;
@@ -100,6 +130,38 @@
     margin: 3px 0 0;
     opacity: .5;
     width: 100%;
+  }
+
+  .settingClass {
+    width: 24px;
+    height: 24px;
+    display: block;
+  }
+
+  .settingMenu {
+    margin: 5px 0;
+    border: 1px solid #d1dbe5;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .12);
+    padding: 6px 0;
+    z-index: 10;
+    position: absolute;
+    top: 50px;
+    background-color: #fff;
+    min-width: 100px;
+    right: 4px
+  }
+
+  .itemClass {
+    list-style: none;
+    line-height: 36px;
+    padding: 0 10px;
+    margin: 0;
+    cursor: pointer;
+    font-weight: 500;
+    color: gray;
+    text-align: center;
+    background-color: #fff;
+    font-size: 15px;
   }
 
   .title-font {
